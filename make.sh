51685ed9536e09c81b89bd177a01524c4da3fefb
turquoise-hexagon <camille@laserbat.pw>
2018-03-29 18:32:11 +0200

    updated : make script

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# ensure bdftopcf is installed
+command -v bdftopcf &> /dev/null ||
+    { echo "error : install bdftopcf first"; exit 1; }
+
 for font in *.bdf; do
     bdftopcf -o "${font/.bdf/.pcf}" "$font";
 done
