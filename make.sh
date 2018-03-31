68191ecc03740afc3fe5f8499da17c9e8f201cb6
MarinHoc <15786638+MarinHoc@users.noreply.github.com>
2018-03-31 10:48:21 +0200

    cleanup

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# ensure bdftopcf is installed
-command -v bdftopcf &> /dev/null ||
-    { echo "error : install bdftopcf first"; exit 1; }
-
-for font in *.bdf; do
-    bdftopcf -o "${font/.bdf/.pcf}" "$font"
-done
