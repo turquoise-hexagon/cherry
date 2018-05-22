30a1af27b3bfd70250478460dbdb5e3d5694f940
camille <camille@lzr.pw>
2018-05-22 19:36:16 +0200

    cleanup

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# ensure bdftopcf is installed
-command -v bdftopcf > /dev/null ||
-    { echo "error : install bdftopcf first"; exit 1; }
-
-for font in *.bdf; do
-    bdftopcf -o "${font%\.*}.pcf" "$font"
-done
