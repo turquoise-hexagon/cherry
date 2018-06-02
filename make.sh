b16ef802dd8967225c0691333b936b9c7c3ee56e
marinhoc <15786638+marinhoc@users.noreply.github.com>
2018-06-02 14:29:40 +0200

    updated : make

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ensure bdftopcf is installed
-if command -v bdftopcf > /dev/null; then
+if ! command -v bdftopcf > /dev/null; then
     echo "error : install bdftopcf first"
     exit 1
 fi
