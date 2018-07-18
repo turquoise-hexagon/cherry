ece8b865e72af5da1843e4f99125347d7a921dc7
camille <camille@lzr.pw>
2018-07-18 13:49:59 +0200

    general updates

---
+++ make.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# ensure bdftopcf is installed
+if ! command -v bdftopcf > /dev/null; then
+    echo "error : install bdftopcf first"
+    exit 1
+fi
+
+for font in *.bdf; do
+    bdftopcf -o "${font%\.*}.pcf" "$font"
+done
