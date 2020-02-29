ccb562812b6b8c2eb69dc38fb851ba0aa01feb51
turquoise-hexagon <camille@lzr.pw>
2020-02-29 22:29:24 +0100

    make make script consistent with all others

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # ensure fonttosfnt is installed
-if ! type fonttosfnt > /dev/null 2>&1; then
+type fonttosfnt > /dev/null 2>&1 || {
     echo 'error : install fonttosfnt first' >&2
     exit 1
-fi
+}
 
 for font in *.bdf; do
     fonttosfnt -v -o "${font%.*}.otb" "$font"
